55287921425d9f2152727c3ebbd298ffee9b87be
Maxim Lisin <lisin.maxim.m@gmail.com>
2021-07-15 10:57:27 +0300

    commit_3

--- Main.java
+++ Main.java
@@ -5,13 +5,33 @@ public class Main {
         int[][] mat = {{1, 2, 3}, {4, 5 ,6}, {7, 8, 9}};
 
         Matrix matrixImpl = new MatrixImpl(mat);
-        System.out.println(matrixImpl.getElement(1, 1));
-
         Matrix transparentMatrix = new TransparentMatrix(new MatrixImpl(mat));
-        System.out.println(transparentMatrix.getElement(2, 2));
-
         Matrix sumMatrices = new SumOfTwoMatrices(new TransparentMatrix(new MatrixImpl(mat)), matrixImpl);
-        System.out.println(sumMatrices.getElement(1,1));
+
+        System.out.println("## Simple matrix");
+        for(int i = 0; i < 3; ++i) {
+            System.out.print("|");
+            for(int j = 0; j < 3; ++j) {
+                System.out.printf("%d ", matrixImpl.getElement(i, j));
+            }
+            System.out.println("|");
+        }
+        System.out.println("## Transparent matrix");
+        for(int i = 0; i < 3; ++i) {
+            System.out.print("|");
+            for(int j = 0; j < 3; ++j) {
+                System.out.printf("%d ", transparentMatrix.getElement(i, j));
+            }
+            System.out.println("|");
+        }
+        System.out.println("## Sum of two matrices");
+        for(int i = 0; i < 3; ++i) {
+            System.out.print("|");
+            for(int j = 0; j < 3; ++j) {
+                System.out.printf("%d ", sumMatrices.getElement(i, j));
+            }
+            System.out.println("|");
+        }
 
 
         
